68710f8128e242fc3ee08b15049a78915c1e4380
Alex Seifert <alex@alexseifert.com>
2024-08-04 13:32:47 +0200

    Start adding posts to the homepage

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,33 @@
+<?php
+$posts_dir = getcwd() . "/content/posts";
+$dirs = glob($posts_dir . '/*' , GLOB_ONLYDIR);
+
+$posts = [];
+
+foreach ($dirs as $dir) {
+  $meta_contents_str = file_get_contents($dir . "/meta.json");
+  $meta_contents = json_decode($meta_contents_str, true);
+  $posts[] = $meta_contents;
+}
+?>
+
 <main class="container">
-  This is the homepage.
+  <?php foreach ($posts as $post): ?>
+    <article class="post">
+      <h2>
+        <a href="/post/<?= $post["id"] ?>"><?= $post["title"] ?></a>
+      </h2>
+
+      <div class="post-meta-data">
+        <?php
+          $date = new DateTimeImmutable($post['publishedDate']);
+          echo $date->format('j F Y');
+        ?>
+      </div>
+
+      <div>
+        <?= $post['excerpt'] ?>
+      </div>
+    </article>
+  <?php endforeach; ?>
 </main>
